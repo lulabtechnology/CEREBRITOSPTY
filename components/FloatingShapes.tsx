cc2b3a2316cfbf2e7c5b6ffb6128b48f6bc54c45
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2026-05-06 22:08:45 -0600

    cd

--- components/FloatingShapes.tsx
+++ components/FloatingShapes.tsx
@@ -6,8 +6,19 @@ export default function FloatingShapes() {
       <div className="absolute bottom-[14%] left-[8%] h-24 w-24 animate-floaty rounded-[35%] bg-limon/45 [animation-delay:2s]" />
       <div className="absolute bottom-[20%] right-[14%] h-14 w-14 animate-wiggle rounded-xl bg-lila/70" />
       <span className="absolute left-[18%] top-[26%] animate-floaty text-3xl [animation-delay:0.5s]">⭐</span>
-      <img src="/images/custom/sticker-rainbow.png" alt="" className="absolute right-[20%] top-[6%] h-10 w-14 animate-floaty object-contain [animation-delay:1.3s]" />
-      <img src="/images/custom/sticker-pencil.png" alt="" className="absolute bottom-[16%] right-[4%] h-14 w-10 animate-floaty object-contain [animation-delay:2.2s]" />
+
+      <img
+        src="/images/custom/sticker-rainbow.png"
+        alt=""
+        className="absolute right-[12%] top-[12%] h-16 w-auto animate-floaty object-contain [animation-delay:1.3s]"
+      />
+
+      <img
+        src="/images/custom/sticker-pencil.png"
+        alt=""
+        className="absolute bottom-[8%] right-[5%] h-20 w-auto animate-floaty object-contain [animation-delay:2.2s]"
+      />
+
       <span className="absolute bottom-[35%] left-[3%] animate-wiggle text-3xl">🧩</span>
     </div>
   );
